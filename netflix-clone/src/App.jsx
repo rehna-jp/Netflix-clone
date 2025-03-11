4a13f6ef083c18ded7f269d5da9dce6c97a7c1c4
rehna-jp <jeremyrehna05@gmail.com>
2025-03-11 12:13:12 +0000

    added the player page

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './pages/Home/Home'
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login/Login'
+import Player from './pages/Player/Player'
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/Login' element={<Login/>}/>
-
+        <Route path='/Player/:id' element={<Player/>}/>
       </Routes>
        
     </div>
--- netflix-clone/src/pages/Player/Player.jsx
+++ netflix-clone/src/pages/Player/Player.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import backarrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
+
 const Player = () => {
+
+  const {id} = useParams();
+
+  const navigate = useNavigate();
+
+
+  const  [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    type: ""
+
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNDkwZTkxYzMxOTIzZTE3ZTRjYzJiMWE1YWQyYzk0MiIsIm5iZiI6MTc0MTYxNjc4NC44MzgsInN1YiI6IjY3Y2VmNjkwMjc5NGIwZDU5ODJhYzZiYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LyAaETjMh3hXCALNFxKYQRbdvJhCGld7VBYM6Jtz2RQ'
+    }
+  };
+  
+   useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results[0]))
+    .catch(err => console.error(err));
+
+   },[])
+  
   return (
     <div className='player'>
-      
+      <img src={backarrow_icon} alt='' onClick={()=>{navigate(-2)}}/>
+      <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`} title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className='player-info'>
+          <p>{apiData.published_at.slice(0,10)}</p>
+          <p>{apiData.name}</p>
+          <p>{apiData.type}</p>
+      </div>
     </div>
   )
 }
